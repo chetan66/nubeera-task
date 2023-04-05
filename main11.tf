2975edc033fa6aaf9792185eed4a7d7477536839
chetan66 <chetandeshmukh1994@gmail.com>
2023-04-05 13:03:39 -0500

    Add files via upload
    Simple code to create S3 bucket using terraform

---
+++ main11.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "************"
+  secret_key = "******************"
+}
+
+# resource "random_pet" "pet_name" {
+#     length = 3
+#     separator = "_"
+# }
+
+# resource "aws_s3_bucket" "pet_bucket" {
+#   bucket ="${random_pet.pet_name.id}-bucket"
+#   acl = "private"
+# }
+
+resource "random_pet" "bucket_name" {
+  length = 2
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-bucket-${random_pet.bucket_name.id}"
+  # other bucket configuration
+}
+output "neededForJenkins" {
+  value = aws_s3_bucket.example.id
+}
+
+
+
+
