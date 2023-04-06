9acbc40f0f823742e14cfa78d277dbd36f254ec0
chetan66 <chetandeshmukh1994@gmail.com>
2023-04-06 07:32:42 -0500

    Add files via upload
    code to install jenkins, java, ansible, maven on ec2 instance

---
+++ nube.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAVON4JPQQAV2KCU4L"
+  secret_key = "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"
+}
+
+resource "aws_instance" "ubuntu-instance515" {
+  ami           = "ami-0aa2b7722dc1b5612"
+  instance_type = "t2.micro"
+  key_name      = "mujahed"
+  vpc_security_group_ids =["sg-080a4f8b835cdbbf4","sg-06b112fa6478a533e"]
+  tags = {
+    Name = "instance212"
+  }
+
+  user_data = <<EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install wget
+                sudo amazon-linux-extras install java-openjdk11
+                sudo amazon-linux-extras install epel -y
+                sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+                sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+                sudo yum install jenkins -y
+                sudo service jenkins start
+              EOF
+}
+output "public_dns" {
+  value = aws_instance.ubuntu-instance515.public_dns
+}
